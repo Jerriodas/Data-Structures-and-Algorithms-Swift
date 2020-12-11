b2af61d4af224ed00792cd491f4e6ba340a18897
Jerriodas <zeus12gerardo@gmail.com>
2020-12-11 14:17:33 -0600

    Linken list struct

---
+++ Linked-List.playground/Sources/LinkedList.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+public struct LinkedList<Value> {
+    public var head: Node<Value>?
+    public var tail: Node<Value>?
+    
+    public init() {}
+    
+    public var isEmpty: Bool {
+        head == nil
+    }
+    
+    public mutating func push(_ value: Value) {
+        head = Node(value: value, next: head)
+        if tail == nil {
+            tail = head
+        }
+    }
+    
+    public mutating func append(_ value: Value) {
+        guard !isEmpty else {
+            push(value)
+            return
+        }
+        tail!.next = Node(value: value)
+        tail = tail!.next
+    }
+}
+
+extension LinkedList: CustomStringConvertible {
+    public var description: String {
+        guard let head = head else {
+            return "Empty List"
+        }
+        return String(describing: head)
+    }
+}
